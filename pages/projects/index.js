a4132d76ceb07f9a46a5290e07509baccf1e20a0
Gabriel Sena <gabriel.sena@smartnx.io>
2022-06-08 18:01:23 -0300

    add certificado bootcamp React

---
+++ pages/projects/index.js
@@ -0,0 +1,8 @@
+import Layout from "../../components/Layout"
+export default function About() {
+    return (
+        <Layout>
+            <a href="/assets/images/react-igti-certificate.jpeg" alt="Desenvolvedor React - Certificado">Certificado Bootcamp React</a>
+        </Layout>
+    )
+}

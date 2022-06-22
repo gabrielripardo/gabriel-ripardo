973eae65f0c843f3a2d475548f07048a5965f875
Gabriel Sena <gabriel.sena@smartnx.io>
2022-06-22 17:24:33 -0300

    add basic Google Analytcs

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,30 @@ import '../styles/globals.css';
 import '../pages/main.scss';
 import 'prismjs/themes/prism-tomorrow.css';
 
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as gtag from '../lib/gtag'
+import Analytics from '../components/Analytics'
+
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <span className="theme-bejamas" />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
